95da87e7b48e1532ff31d6d91629848f970f70e7
Ryan <ryanevans253@yahoo.com>
2020-12-28 20:45:33 -0500

    Second chart added

--- calculator.js
+++ calculator.js
@@ -1,9 +1,21 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
+    title: 'myfjdksafdsa',
     options: {
-        cutoutPercentage: 40,
+        cutoutPercentage: 30,
+        legend: {
+            position: 'bottom',
+        },
+        title: {
+            display: true,
+            text: 'Expense Breakdown',
+            fontSize: 30,
+            padding: 10,
+            fontColor: 'rgba(95, 91, 255, 1)',
+        },   
     },
+
     data: {
         labels: ['Mortgage', 'Property Tax', 'Insurance', 'Maintenance', 'CapEx', 'Vacancy', 'Property Management', 'Utilities'],
         
@@ -23,6 +35,41 @@ var myChart = new Chart(ctx, {
             borderWidth: 0
         }]
     },    
+});
+
+// second chart for flips
+var flipper = document.getElementById('flipChart').getContext('2d');
+var flipChart = new Chart(flipper, {
+    type: 'doughnut',
+    options: {
+        cutoutPercentage: 25,
+        title: {
+            display: true,
+            text: 'Fix and Flip Expenses',
+            fontSize: 30,
+            padding: 10,
+            fontColor: 'rgba(95, 91, 255, 1)',
+        },
+    },
+    data: {
+        labels: ['Purchase Price', 'Purchase Closing Costs', 'Sale Closing Costs', 'Agent Commission', 'Repair Costs', 'Holding Cost'],
+
+        datasets: [{
+            label: '% of Sales Price',
+            data: [30,10,4,18,7,13],
+            backgroundColor: [
+                'rgba(62, 162, 168)',
+                'rgba(240, 90, 31)',
+                'rgba(255, 198, 46)',
+                'rgba(92, 92, 91)',
+                'rgba(163, 207, 209)',
+                'rgba(248, 160, 112)',
+                'rgba(255, 228, 166)',
+                'rgba(155, 158, 160)'
+            ],
+            borderWidth: 0,
+        }]
+    },
 });
 
 
@@ -64,7 +111,7 @@ function updateMortgage() {
     var testnumber = document.getElementById('monthlyPayment');
     return monthlyPayment;
 }
-
+// shouldn't be necessary code. i think this was a test
 function changeSize(chart) {
     chart.options.cutoutPercentage = 70;
     chart.update();
