9a5d2480cfab71e9bd391da2988df3f2723ec4b2
Ryan <ryanevans253@yahoo.com>
2021-02-28 19:04:01 -0500

    added advanced options

--- js/mortgage.js
+++ js/mortgage.js
@@ -15,21 +15,33 @@ function calculateMonthlyMortgage() {
     document.getElementById('monthlyMortgagePayment').innerHTML = "$" + monthlyPayment.toFixed(0);
 }
 
-function calculateAdvancedOptionsj() {
+function calculateAdvancedOptions() {
     let propertyTaxes = parseFloat(document.getElementById('propertyTaxes').value) / 12;
     let propertyInsurance = parseFloat(document.getElementById('propertyInsurance').value) / 12;
     let hoaFees = parseFloat(document.getElementById('HOAFees').value) / 12; 
+
+    let homePrice = parseFloat(document.getElementById('homePrice').value);
+    let downPayment = parseFloat(document.getElementById('downPayment').value);
+
+    let monthlyPayment;
+    let principal = homePrice - downPayment;
+    let interestRate = (parseFloat(document.getElementById('interestRate').value) / 12) / 100;
+    let numberOfPayments = parseFloat(document.getElementById('loanLength').value) * 12; 
+    let percentageRate = interestRate.toFixed(4);
+
+
+    monthlyPayment = principal * interestRate * (Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+
+    let totalMonthlyPayment = monthlyPayment + propertyTaxes + propertyInsurance + hoaFees;
+
+
+    document.getElementById('monthlyMortgagePayment').innerHTML = "$" + totalMonthlyPayment.toFixed(0);
+
+
 }
 
 
-// function showOptions() {
-//   var x = document.getElementById("advancedOptions");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
+
 
 
 function showOptions() {
@@ -40,3 +52,5 @@ function showOptions() {
     x.style.display = "block";
   }
 }
+
+
