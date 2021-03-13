eb6c8eee6b43c668094f29f1c928ceeb064ff8f6
Ryan <ryanevans253@yahoo.com>
2021-03-13 09:02:52 -0500

    added mortgage chart

--- js/mortgage.js
+++ js/mortgage.js
@@ -37,7 +37,10 @@ function calculateAdvancedOptions() {
 
     document.getElementById('monthlyMortgagePayment').innerHTML = "$" + totalMonthlyPayment.toFixed(0);
 
+}
 
+function principalAndInterest () {
+    let prin
 }
 
 
@@ -54,3 +57,48 @@ function showOptions() {
 }
 
 
+var donutMortgageChart = document.getElementById('mortgageChart').getContext('2d');
+var mortgageChart = new Chart(donutMortgageChart, {
+    type: 'doughnut',
+    options: {
+        cutoutPercentage: 25,
+        legend: {
+            position: 'bottom',
+        },
+        title: {
+            display: true,
+            text: 'Payment Breakdown',
+            fontSize: 30,
+            padding: 10,
+            fontColor: 'rgba(95, 91, 255, 1)',
+        },
+    },
+    data: {
+        labels: ['Principal and Interest', 'Insurance', 'Property Tax', 'HOA Fees'],
+
+        datasets: [{
+            label: '% of Sales Price',
+            data: [1,1,1,1],
+            backgroundColor: [
+                'rgba(97,245,245)',                        // 'rgba(62, 162, 168)',
+                'rgba(98,195,245)',                        // 'rgba(240, 90, 31)',
+                'rgba(97,147,245)',                        // 'rgba(255, 198, 46)',
+                'rgba(98,96,244)',                        // 'rgba(92, 92, 91)',
+                // 'rgba(99,49,245)',                        // 'rgba(163, 207, 209)',
+                                        // 'rgba(248, 160, 112)',
+                                        // 'rgba(255, 228, 166)',
+                                        // 'rgba(155, 158, 160)'
+            ],
+            borderWidth: 0,
+        }]
+    },
+});
+
+
+function updateAll(chart) {
+    chart.data.datasets[0].data[0] = document.getElementById('inputPurchaseClosingCosts').value;
+    chart.data.datasets[0].data[1] = document.getElementById('inputSaleClosingCosts').value;
+    chart.data.datasets[0].data[2] = document.getElementById('inputRepair').value;
+    chart.data.datasets[0].data[3] = calculateCommission().toFixed(0);
+    chart.update();
+}
