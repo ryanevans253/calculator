61a15a0f002cc399234c85034cb5f93b2682379d
Ryan <ryanevans253@yahoo.com>
2021-11-03 01:10:50 -0400

    refactored calcAll

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,12 +1,16 @@
-let homePrice = parseFloat(document.getElementById('homePrice').value);
-let downPayment = parseFloat(document.getElementById('downPayment').value);
+"use strict";
 
-let monthlyPayment;
-let principal = homePrice - downPayment;
+// let homePrice = parseFloat(document.getElementById("homePrice").value);
+// let downPayment = parseFloat(document.getElementById("downPayment").value);
 
-let propertyTaxes = parseFloat(document.getElementById('propertyTaxes').value) / 12;
-let propertyInsurance = parseFloat(document.getElementById('propertyInsurance').value) / 12;
-let hoaFees = parseFloat(document.getElementById('HOAFees').value) / 12; 
+// let monthlyPayment;
+// let principal = homePrice - downPayment;
+
+// let propertyTaxes =
+//   parseFloat(document.getElementById("propertyTaxes").value) / 12;
+// let propertyInsurance =
+//   parseFloat(document.getElementById("propertyInsurance").value) / 12;
+// let hoaFees = parseFloat(document.getElementById("HOAFees").value) / 12;
 
 // function calculateInsurance() {
 //     let propertyInsurance = parseFloat(document.getElementById('propertyInsurance').value) / 12;
@@ -15,78 +19,117 @@ let hoaFees = parseFloat(document.getElementById('HOAFees').value) / 12;
 // }
 
 function calculateMonthlyMortgage() {
- 
-    let interestRate = (parseFloat(document.getElementById('interestRate').value) / 12) / 100;
-    let numberOfPayments = parseFloat(document.getElementById('loanLength').value) * 12; 
-    let percentageRate = interestRate.toFixed(4);
+  let interestRate =
+    parseFloat(document.getElementById("interestRate").value) / 12 / 100;
+  let numberOfPayments =
+    parseFloat(document.getElementById("loanLength").value) * 12;
+  let percentageRate = interestRate.toFixed(4);
 
-    monthlyPayment = principal * interestRate * (Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+  monthlyPayment =
+    (principal * interestRate * Math.pow(1 + interestRate, numberOfPayments)) /
+    (Math.pow(1 + interestRate, numberOfPayments) - 1);
 
-    document.getElementById('monthlyMortgagePayment').innerHTML = "$" + monthlyPayment.toFixed(0);
-    return monthlyPayment
+  document.getElementById("monthlyMortgagePayment").innerHTML =
+    "$" + monthlyPayment.toFixed(0);
+  return monthlyPayment;
 }
 
 function calculateAdvancedOptions() {
-    let homePrice = parseFloat(document.getElementById('homePrice').value);
-    let downPayment = parseFloat(document.getElementById('downPayment').value);
-    let monthlyPayment;
-    let principal = homePrice - downPayment;
-    let interestRate = (parseFloat(document.getElementById('interestRate').value) / 12) / 100;
-    let numberOfPayments = parseFloat(document.getElementById('loanLength').value) * 12; 
-    let percentageRate = interestRate.toFixed(4);
+  let homePrice = parseFloat(document.getElementById("homePrice").value);
+  let downPayment = parseFloat(document.getElementById("downPayment").value);
+  let monthlyPayment;
+  let principal = homePrice - downPayment;
+  let interestRate =
+    parseFloat(document.getElementById("interestRate").value) / 12 / 100;
+  let numberOfPayments =
+    parseFloat(document.getElementById("loanLength").value) * 12;
+  let percentageRate = interestRate.toFixed(4);
 
-    monthlyPayment = principal * interestRate * (Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+  monthlyPayment =
+    (principal * interestRate * Math.pow(1 + interestRate, numberOfPayments)) /
+    (Math.pow(1 + interestRate, numberOfPayments) - 1);
 
-    let totalMonthlyPayment = monthlyPayment + propertyTaxes + propertyInsurance + hoaFees;
-    document.getElementById('monthlyMortgagePayment').innerHTML = "$" + totalMonthlyPayment.toFixed(0);
+  let totalMonthlyPayment =
+    monthlyPayment + propertyTaxes + propertyInsurance + hoaFees;
+  document.getElementById("monthlyMortgagePayment").innerHTML =
+    "$" + totalMonthlyPayment.toFixed(0);
 }
 
-function showOptions() {
-  var x = document.getElementById("advancedOptions");
-  if (x.style.display !== "none") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-var donutMortgageChart = document.getElementById('mortgageChart').getContext('2d');
+var donutMortgageChart = document
+  .getElementById("mortgageChart")
+  .getContext("2d");
 var mortgageChart = new Chart(donutMortgageChart, {
-    type: 'doughnut',
-    options: {
-        cutoutPercentage: 25,
-        legend: {
-            position: 'bottom',
-        },
-        title: {
-            display: true,
-            text: 'Payment Breakdown',
-            fontSize: 30,
-            padding: 10,
-            fontColor: 'rgba(95, 91, 255, 1)',
-        },
+  type: "doughnut",
+  options: {
+    cutoutPercentage: 25,
+    legend: {
+      position: "bottom",
     },
-    data: {
-        labels: ['Principal and Interest', 'Insurance', 'Property Tax', 'HOA Fees'],
-        datasets: [{
-            label: '% of Sales Price',
-            data: [1,1,1,1],
-            backgroundColor: [
-                'rgba(97,245,245)',                        
-                'rgba(98,195,245)',                       
-                'rgba(97,147,245)',                       
-                'rgba(98,96,244)',                       
-            ],
-            borderWidth: 0,
-        }]
+    title: {
+      display: true,
+      text: "Payment Breakdown",
+      fontSize: 30,
+      padding: 10,
+      fontColor: "rgba(95, 91, 255, 1)",
     },
+  },
+  data: {
+    labels: ["Principal and Interest", "Insurance", "Property Tax", "HOA Fees"],
+    datasets: [
+      {
+        label: "% of Sales Price",
+        data: [1, 1, 1, 1],
+        backgroundColor: [
+          "rgba(97,245,245)",
+          "rgba(98,195,245)",
+          "rgba(97,147,245)",
+          "rgba(98,96,244)",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  },
 });
 
-// need to calculate these numbers after button is pushed. currently calculating on pageload. 
+// need to calculate these numbers after button is pushed. currently calculating on pageload.
 function updateAll(chart) {
-    chart.data.datasets[0].data[0] = calculateMonthlyMortgage().toFixed(0);
-    chart.data.datasets[0].data[1] = propertyInsurance;
-    chart.data.datasets[0].data[2] = propertyTaxes;
-    chart.data.datasets[0].data[3] = hoaFees;
-    chart.update();
+  chart.data.datasets[0].data[0] = calculateMonthlyMortgage().toFixed(0);
+  chart.data.datasets[0].data[1] = propertyInsurance;
+  chart.data.datasets[0].data[2] = propertyTaxes;
+  chart.data.datasets[0].data[3] = hoaFees;
+  chart.update();
+}
+
+function toggleAdvancedOptions() {
+  document.getElementById("advancedOptions").classList.toggle("hidden");
+}
+
+// function getter(id) {
+//   let inputLabel = document.getElementById(`${id}`);
+//   //   return inputLabel;
+//   console.log(id.id);
+// }
+
+// function calcAdvanced() {
+//   let tester = getter(propertyInsurance);
+//   console.log(HOAFees.value);
+//   console.log(tester);
+// }
+
+function calcAll() {
+  const principal1 = homePrice.value - downPayment.value;
+  const interestRate1 = interestRate.value / 12 / 100;
+  const numberOfPayments1 = loanLength.value * 12;
+  const options =
+    (+propertyTaxes.value + +propertyInsurance.value + +HOAFees.value) / 12; //unary operator
+  let monthlyPayment =
+    (principal1 *
+      interestRate1 *
+      Math.pow(1 + interestRate1, numberOfPayments1)) /
+    (Math.pow(1 + interestRate1, numberOfPayments1) - 1);
+  document.getElementById("monthlyMortgagePayment").innerHTML =
+    "$" + (options + monthlyPayment).toFixed(0);
+  document.getElementById("pAndI").innerHTML = "$" + monthlyPayment.toFixed(0);
+
+  return monthlyPayment + options;
 }
